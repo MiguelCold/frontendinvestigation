61a4e5af3433e1c5d1c25562daf37bfb3817ec9a
Miguel Angel Ossa Ruiz <miguelcold8@gmail.com>
2014-10-23 10:29:29 -0500

    -Adding the angular js. -Adding the angular js module. -Adding the device list in html view. -Adding the json array devices.

---
+++ AngularExample/WebContent/resources/js/module/angularexample.js
@@ -0,0 +1,62 @@
+var mockDataForThisTest = "json=" + encodeURI(JSON.stringify([
+    {
+    	id: 1,
+    	name: "Samsung Galaxy S3",
+    	description: "Smartphone",
+    	image: "samsun_galaxy_s3.png",
+    	manufacturer: "Samsung",
+    	price: 900000
+    },
+    {
+    	id: 2,
+    	name: "Huawei Acend G610",
+    	description: "Smartphone",
+    	image: "huawei_acend_g610.png",
+    	manufacturer: "Huawei",
+    	price: 360000
+    }
+]));
+
+
+// / *** MODULES *** / //
+
+var angularExampleModule = angular.module('angularExampleModule', [ 'ngRoute' ]);
+
+
+/// *** CONTROLLERS *** / //
+
+angularExampleModule.controller('deviceListController', ['$scope', function(scope) {
+    scope.devices = [ 
+    {
+    	id: 1,
+    	name: "Samsung Galaxy S3",
+    	description: "Smartphone",
+    	image: "samsun_galaxy_s3.png",
+    	manufacturer: "Samsung",
+    	price: 900000
+    },
+    {
+    	id: 2,
+    	name: "Huawei Acend G610",
+    	description: "Smartphone",
+    	image: "huawei_acend_g610.png",
+    	manufacturer: "Huawei",
+    	price: 360000
+    }];
+}]);
+
+/// *** CONFIGURATIONS *** / //
+
+angularExampleModule.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : './pages/devicelist.html',
+		controller : 'deviceListController'
+	});
+} ]);
+
+// / *** RUNNERS *** / //
+angularExampleModule.run(function($rootScope, $location) {
+	$rootScope.$on('$routeChangeStart', function() {
+		$location.url('/');
+	});
+});
