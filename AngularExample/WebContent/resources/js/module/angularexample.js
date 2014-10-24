1ab6541e4f56f91cc8c403f6be0fb980eea1158d
Miguel Angel Ossa Ruiz <miguelcold8@gmail.com>
2014-10-24 03:16:25 -0500

    -Adding the new view, add new device on the list. -Adding the boostrap for the style.

--- AngularExample/WebContent/resources/js/module/angularexample.js
+++ AngularExample/WebContent/resources/js/module/angularexample.js
@@ -3,27 +3,97 @@
 
 var angularExampleModule = angular.module('angularExampleModule', [ 'ngRoute' ]);
 
+// / *** VARIABLES *** / //
+
+var id = 1;
+var names = ['LG G3', 'Sony Xperia Z3 Compact', 'LG G2', 'HTC One (M8)'];
+var memoryRAMs = ['2GB RAM', '1GB RAM', '1.5GB RAM', '1.8GB RAM'];
+var processors = ['Samsung Exynos 5 Octa', 'Qualcomm Snapdragon 800',
+                 'Qualcomm Snapdragon 600' , 'NVidia Tegra 4' , 
+                 'NVidia Tegra 4i', 'Intel Atom Z2580 Clover Trail', 
+                 'Intel Atom Z2460']
+var internMemories = ['16GB RAM', '32GB RAM', '4GB RAM', '2GB RAM']
+var years = [2011, 2010, 2014, 2013]
+var prices = [1578793.04, 1150310.59, 1812810.38, 1697449.72, 754788.32]
 
 /// *** CONTROLLERS *** / //
 
-angularExampleModule.controller('deviceListController', ['$scope', function(scope) {
-    scope.devices = [ 
-    {
-    	id: 1,
-    	name: "Samsung Galaxy S3",
-    	description: "Smartphone",
-    	image: "samsun_galaxy_s3.png",
-    	manufacturer: "Samsung",
-    	price: 900000
-    },
-    {
-    	id: 2,
-    	name: "Huawei Acend G610",
-    	description: "Smartphone",
-    	image: "huawei_acend_g610.png",
-    	manufacturer: "Huawei",
-    	price: 360000
-    }];
+angularExampleModule.controller('deviceListController', ['$scope', '$location',
+                                                         function($scope, $location) {
+
+
+	  function generateRandomDevice(id) {
+	        var name = names[Math.floor(Math.random() * 3)];
+	        var memoryRAM = memoryRAMs[Math.floor(Math.random() * 3)];
+	        var processor = processors[Math.floor(Math.random() * 6)];
+	        var internMemory = internMemories[Math.floor(Math.random() * 3)];
+	        var year = years[Math.floor(Math.random() * 3)];
+	        var price = prices[Math.floor(Math.random() * 4)];
+	        return {
+	            id: id,
+	            name: name,
+	            memoryRAM: memoryRAM,
+	            processor: processor,
+	            internMemory: internMemory,
+	            year: year,
+	            price: price
+	        }
+	    }
+	  
+	  function generateNewDevice(id, name, memoryRAM, processor, 
+			  internMemory, year, price) {
+		  if((name==null || name=='')
+				  ||(memoryRAM==null || memoryRAM=='')
+				  ||(processor==null || processor=='')
+				  ||(internMemory==null || internMemory=='')
+				  ||(year==0)||(price==0)){
+			  return null
+		  }else{
+	        return {
+	            id: id,
+	            name: name,
+	            memoryRAM: memoryRAM,
+	            processor: processor,
+	            internMemory: internMemory,
+	            year: year,
+	            price: price
+	        }
+		  }
+	    }
+	  
+	  function clearData(){
+		  $scope.name='';
+		  $scope.memoryRAM=''; 
+		  $scope.processor=''; 
+		  $scope.internMemory=''; 
+		  $scope.year=0; 
+		  $scope.price=0;
+	  }
+	    $scope.addNewDevice = function addNewDevice() {
+	    	var newDevice = generateNewDevice(id, $scope.name, 
+	    			$scope.memoryRAM, $scope.processor, $scope.internMemory, 
+	    			$scope.year, $scope.price);
+	    	if(newDevice!=null){
+		    	$scope.rowCollection.push(newDevice);
+				$scope.displayedCollection = [].concat($scope.rowCollection);
+		        id++;
+		        clearData()
+	    	}
+	    };
+
+	    $scope.removeDevice = function removeDevice(device) {
+	        var index = $scope.rowCollection.indexOf(device);
+	        if (index !== -1) {
+	        	$scope.rowCollection.splice(index, 1);
+	        	$scope.displayedCollection = [].concat($scope.rowCollection);
+	        }
+	    }
+	    
+		 $scope.rowCollection = [];
+		 for (id; id < 5; id++) {
+		    $scope.rowCollection.push(generateRandomDevice(id));
+		 }
+		 $scope.displayedCollection = [].concat($scope.rowCollection);
 }]);
 
 /// *** CONFIGURATIONS *** / //
@@ -38,6 +108,6 @@ angularExampleModule.config([ '$routeProvider', function($routeProvider) {
 // / *** RUNNERS *** / //
 angularExampleModule.run(function($rootScope, $location) {
 	$rootScope.$on('$routeChangeStart', function() {
-		$location.url('/');
+			$location.url('/');
 	});
 });
